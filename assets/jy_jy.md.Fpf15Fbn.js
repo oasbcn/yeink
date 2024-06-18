10d1f57d6496c908302ece02d2258e536fecd6c6
“12yyyyy” <2475201762@qq.com>
2024-06-18 15:40:57 +0800

    个人知识库正式版0.0.5💥

---
+++ assets/jy_jy.md.Fpf15Fbn.js
@@ -0,0 +1,23 @@
+import{_ as s,c as a,o as n,a3 as e}from"./chunks/framework.ZS1GtFyN.js";const b=JSON.parse('{"title":"VitePress","description":"","frontmatter":{"layout":"doc"},"headers":[],"relativePath":"jy/jy.md","filePath":"jy/jy.md","lastUpdated":null}'),p={name:"jy/jy.md"},t=e(`<h1 id="vitepress" tabindex="-1">VitePress <a class="header-anchor" href="#vitepress" aria-label="Permalink to &quot;VitePress&quot;">​</a></h1><p>由 Vite 和 Vue 驱动的静态站点生成器</p><p>1、VitePress框架部署(如下)⇓⇓⇓           <a href="/jy/github/githubcode-update.html">2、Github仓库的创建和文件的上传与更新</a></p><h2 id="介绍" tabindex="-1">介绍 <a class="header-anchor" href="#介绍" aria-label="Permalink to &quot;介绍&quot;">​</a></h2><p>12yyyyy-Docs：耶系-资源-经验社区-贰耶的开源项目文档</p><h2 id="软件架构" tabindex="-1">软件架构 <a class="header-anchor" href="#软件架构" aria-label="Permalink to &quot;软件架构&quot;">​</a></h2><p><code>vitepress</code></p><h2 id="环境" tabindex="-1">环境 <a class="header-anchor" href="#环境" aria-label="Permalink to &quot;环境&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>npm install -D vitepress</span></span></code></pre></div><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>yarn add -D vitepress</span></span></code></pre></div><h2 id="向导" tabindex="-1">向导 <a class="header-anchor" href="#向导" aria-label="Permalink to &quot;向导&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>npx vitepress init</span></span></code></pre></div><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>T  Welcome to VitePress!</span></span>
+<span class="line"><span>|</span></span>
+<span class="line"><span>o  Where should VitePress initialize the config?</span></span>
+<span class="line"><span>|  ./</span></span>
+<span class="line"><span>|</span></span>
+<span class="line"><span>o  Site title:</span></span>
+<span class="line"><span>|  12yyyyy</span></span>
+<span class="line"><span>|</span></span>
+<span class="line"><span>o  Site description:</span></span>
+<span class="line"><span>|  TB</span></span>
+<span class="line"><span>|</span></span>
+<span class="line"><span>o  Theme:</span></span>
+<span class="line"><span>|  Default Theme</span></span>
+<span class="line"><span>|</span></span>
+<span class="line"><span>o  Use TypeScript for config and theme files?</span></span>
+<span class="line"><span>|  Yes</span></span>
+<span class="line"><span>|</span></span>
+<span class="line"><span>o  Add VitePress npm scripts to package.json?</span></span>
+<span class="line"><span>|  Yes</span></span>
+<span class="line"><span>|</span></span>
+<span class="line"><span>—  Done! Now run npm run docs:dev and start writing.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>PS C:\\Users\\HA\\Desktop\\my-1&gt;</span></span></code></pre></div><h2 id="开发" tabindex="-1">开发 <a class="header-anchor" href="#开发" aria-label="Permalink to &quot;开发&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>npm run docs:dev</span></span></code></pre></div><h2 id="编译" tabindex="-1">编译 <a class="header-anchor" href="#编译" aria-label="Permalink to &quot;编译&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>npm run docs:build</span></span></code></pre></div>`,17),i=[t];function l(o,c,d,r,h,u){return n(),a("div",null,i)}const v=s(p,[["render",l]]);export{b as __pageData,v as default};
